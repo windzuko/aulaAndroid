f3319b4ce5716c3219dd12d8098fdc7f6e66db05
WindZuko <11465Abc@>
2023-08-09 20:21:53 -0300

    commit2

--- app/src/main/java/com/example/aulaandroid/MainActivity.kt
+++ app/src/main/java/com/example/aulaandroid/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.aulaandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/example/aulaandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/example/aulaandroid/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.aulaandroid
 
 import org.junit.Test
 
